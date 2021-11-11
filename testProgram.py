85f5a6d51c0be4eef2944cf061a8fdb3153048a7
ToThrSix <89365751+ToThrSix@users.noreply.github.com>
2021-11-11 15:56:39 +0900

    Create testProgram.py
    first test for own .exe program

---
+++ testProgram.py
@@ -0,0 +1,24 @@
+# Practice to make a private program.
+import sys
+import PyQt5.QtWidgets import QApplication, QWidget
+
+class testProgram(QWidget):
+
+    def __init__(self):
+    
+        super().__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+    
+        self.setWindowTitle("This is my first program.")
+        self.move(300, 300)
+        self.resize(500, 400)
+        self.show()
+        
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ex = testProgram()
+    sys.exit(app.exec_())
