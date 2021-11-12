9d25730749aad8b9dbed83d9cb9e9b90c57cb686
ToThrSix <89365751+ToThrSix@users.noreply.github.com>
2021-11-12 17:57:42 +0900

    audio source_20211112(1)

--- testProgram6.py
+++ testProgram6.py
@@ -1,1 +1,17 @@
 # To Control the other programs volume, use pyaudio
+
+import pyaudio
+import wave
+import sys
+
+CHUNK = 1024
+
+if len(sys.argv) < 2:
+
+    print("Plays a .wav file. \n\n Usage: %s.wav" % sys.argv[0])
+    sys.exit(-1)
+
+wf = wave.open(sys.argv[1], "rb")
+p = pyaudio.PyAudio()
+
+stream = p.open()
