cc0ecde3735d9e42d971b07289b2063c9ebccc82
ToThrSix <89365751+ToThrSix@users.noreply.github.com>
2021-11-16 09:11:15 +0900

    practice_20211116(1)

--- testProgram10.py
+++ testProgram10.py
@@ -36,4 +36,17 @@ class testProgram10(QMainWindow):
 
         self.setLayout(vbox)
 
+        self.setWindowTitle("Label Test")
+        self.setGeometry(300, 300, 500, 400)
+        self.show()
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ex = testProgram10()
+
+    # python 2.7 기준으로는 exec()와 exec_()가 서로 다른 함수였지만
+    # python 3.x 기준으로는 같은 함수이다.
+    sys.exit(app.exec_())
+
         
